f0ea4d6bbda41f14a0d7ba3da9cf1458f0cd0a03
snyderg13 <georgesny13@gmail.com>
2025-07-27 16:12:52 -0400

    add some todo comments

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import sys
 def main():
     print("Hello from opencv-test!")
     
+    # @TODO: figure out how to get better detect
+    #        available cams and choose desired
     cap = cv2.VideoCapture(0)
     if not cap.isOpened():
         print("Err: Could not open cam")
@@ -11,6 +13,7 @@ def main():
     
     print("Cam opened successfully")
     
+    # @TODO: err/exc handling to get more info
     while True:
         ret, frame = cap.read()
         if not ret:
